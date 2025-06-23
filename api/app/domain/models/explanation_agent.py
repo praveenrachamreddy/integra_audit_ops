af54561306d82a4bdfdc645349723bcf2124b6ca
anthony-okoye <tagboanthonyokoye@gmail.com>
2025-06-23 13:38:01 +0100

    implementing github test

--- api/app/domain/models/explanation_agent.py
+++ api/app/domain/models/explanation_agent.py
@@ -3,7 +3,7 @@ from typing import Any, Dict
 
 class ExplanationRequest(BaseModel):
     query: Dict[str, Any]
-
+ 
 class ExplanationResponse(BaseModel):
     adk_result: dict
     llm_result: str 
